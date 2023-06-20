b77edd8135c4705b93bc2f05cd349f62c6064a7c
Ciberian <isi04047891@gmail.com>
2023-06-20 06:43:47 +0500

    Подготавливает рабочее окружение, добавляет вспомогательные компоненты

---
+++ app/signin/page.tsx
@@ -0,0 +1,8 @@
+export default function SignIn() {
+  return (
+    <div className="stack">
+      <h1 className="home-title">Sign in</h1>;
+    </div>
+  );
+  
+}
---
+++ components/Footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        Проект создан на основе <a href="https://www.youtube.com/watch?v=fDesagJgw3w&t=356s">видеоурока</a>.
+      </p>
+      <p>
+        Документация <a href="https://next-auth.js.org/getting-started/introduction">NextAuth.js</a> и{' '}
+        <a href="https://authjs.dev/getting-started/oauth-tutorial">Auth.js</a>
+      </p>
+    </footer>
+  );
+};
+
+export { Footer };
